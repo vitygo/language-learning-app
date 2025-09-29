fb0c462741601ebc362af766dc1e62b8b9f3330e
Viktor <vitygocanal@gmail.com>
2025-09-29 22:46:20 +0200

    add level page

--- src/pages/LevelPage.jsx
+++ src/pages/LevelPage.jsx
@@ -1,7 +1,27 @@
+import "./LevelPage.css";
+import { useSelector, useDispatch } from "react-redux";
+import { IoClose } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
+export default function LevelPage() {
+  const heart = useSelector((state) => state.heart.value);
 
-export default function LevelPage(){
-    return(
-        <div className="level">level</div>
-    )
+  return (
+    <div className="level">
+      <div className="level__header">
+        <div className="header__button">
+          <Link to="/dashboard">
+            <IoClose size="30px" color="grey" />
+          </Link>
+        </div>
+        <div className="header__level-complete">
+          <div className="level-complete__line"></div>
+        </div>
+        <div className="header__heart">
+          <img src="../heart.png" className="heart__img" />
+          <p className="elements__data data--heart">{heart}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
